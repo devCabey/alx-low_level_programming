5f09f46f33dc6067303624030c06d11231f7e28e
Caleb Okyere <agyakojo4321@gmail.com>
2023-03-17 22:28:47 -0700

    error with betty resolved

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,35 +8,36 @@
 
 int main(void)
 {
-        int f;
-        int s;
-        int t;
+	int f;
+	int s;
+	int t;
 	int fo;
 
-        for (f = 0; f < 10; f++)
-        {
-                for (s = 0; s < 10; s++)
-                {
-                 	for (t = f; t < 10; t++)
-                	{
-                        	for (fo = s + 1; fo < 10; fo++)
-                        	{
-                                	putchar(48 + f);
+	for (f = 0; f < 10; f++)
+	{
+		for (s = 0; s < 10; s++)
+		{
+			for (t = f; t < 10; t++)
+			{
+				for (fo = s + 1; fo < 10; fo++)
+				{
+					putchar(48 + f);
 					putchar(48 + s);
 					putchar(' ');
-                                	putchar(48 + t);
-                                	putchar(48 + fo);
-                                	if (f == 9 && s == 8 && t == 9 && fo == 9)
-                                	{}
-                                	else
-                                	{
-                                        	putchar(',');
-                                        	putchar(' ');
-                                	}
-                        	}
-                	}
-                }
-        }
-        putchar('\n');
-        return (0);
+					putchar(48 + t);
+					putchar(48 + fo);
+					if (f == 9 && s == 8 && t == 9 && fo == 9)
+					{}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
 }
