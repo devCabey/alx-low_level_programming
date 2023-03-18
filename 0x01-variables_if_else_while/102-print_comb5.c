a957a0fecffdce05ae14ac54c01e8ea26b8d9546
Caleb Okyere <agyakojo4321@gmail.com>
2023-03-17 21:48:55 -0700

    resolving error

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+        int f;
+        int s;
+        int t;
+	int fo;
+
+        for (f = 0; f < 10; f++)
+        {
+                for (s = 0; s < 10; s++)
+                {
+                 	for (t = 0; t < 10; t++)
+                	{
+                        	for (fo = 1; fo < 10; fo++)
+                        	{
+                                	putchar(48 + f);
+					putchar(48 + s);
+					putchar(' ');
+                                	putchar(48 + t);
+                                	putchar(48 + fo);
+                                	if (f == 9 && s == 8 && t == 9 && fo == 9)
+                                	{}
+                                	else
+                                	{
+                                        	putchar(',');
+                                        	putchar(' ');
+                                	}
+                        	}
+                	}
+                }
+        }
+        putchar('\n');
+        return (0);
+}
