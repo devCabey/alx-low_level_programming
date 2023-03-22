fc298df8295ba2152b79f3eb23ab840a5005b1b8
Caleb Okyere <agyakojo4321@gmail.com>
2023-03-21 19:35:59 -0700

    C program that print the alphabet in lower case using a prototype print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alph = print_alphabet();
+
+	_putchar(alph);
+
+	return (0);
+}
