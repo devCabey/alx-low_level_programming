3da45d675bb1b7f8ecf2cfe58bcdbdcbdf3529f6
Caleb Okyere <agyakojo4321@gmail.com>
2023-03-22 17:03:12 -0700

    error with 7-* file resolved

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,21 +12,9 @@ int print_last_digit(int num)
 {
 	int last_digit;
 
-	if (num > 0)
-	{
-		last_digit = num % 10;
-		_putchar(last_digit);
-		return (last_digit);
-	}
-	else if (num < 0)
-	{
-		last_digit = num % 10;
-		_putchar(-last_digit);
-		return (-last_digit);
-	}
-	else
-	{
-		_putchar(0);
-		return (0);
-	}
+	if (num < 0)
+		last_digit *= -1;
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
