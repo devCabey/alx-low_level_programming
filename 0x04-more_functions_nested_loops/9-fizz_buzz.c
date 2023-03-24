7b8f6e4a0bf151de66b42fafc1c23485e28274bd
Caleb Okyere <agyakojo4321@gmail.com>
2023-03-24 09:25:14 -0700

    Program to print 1 to 100

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Prints 1 to 100, print fizz, buzz and fizzbuzz
+ *
+ * Return: 0 always (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		i++;
+	}
+	printf("\n");
+
+	return (0);
+}
