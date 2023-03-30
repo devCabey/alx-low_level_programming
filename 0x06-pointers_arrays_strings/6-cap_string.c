0c5b96ae7b0a636553c4f5ffc7c94a0e83ff2227
Caleb Okyere <agyakojo4321@gmail.com>
2023-03-30 07:10:36 -0700

    done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -29,10 +29,6 @@ char *cap_string(char *c)
 		}
 		if (c[i] == '\t' || c[i] == '\n')
 		{
-			if (c[i] == '\t')
-			{
-				c[i] = (char) 32;
-			}
 			if (c[i + 1] > 96 && c[i + 1] < 123)
 			{
 				j = c[i + 1] - 32;
