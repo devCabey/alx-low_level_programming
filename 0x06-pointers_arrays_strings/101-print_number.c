66a28cf65604aa7c8ada6556509bba3f16bbaa11
Caleb Okyere <agyakojo4321@gmail.com>
2023-03-31 04:48:39 -0700

    testing code with checker

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * recurse - Print integer
+ *
+ * @i: Unsigned Integer
+ *
+ * Return: void
+ */
+void recurse(unsigned int i)
+{
+	unsigned int j, k;
+
+	j  = i / 10;
+	k = i % 10;
+	if (j >= 10)
+	{
+		recurse(j);
+		_putchar(k + '0');
+	}
+	else
+	{
+		_putchar(j + '0');
+	}
+}
+/**
+ * print_number - Prints an integer
+ *
+ * @n: Integer value
+ *
+ * Return: Void
+ */
+void print_number(int n)
+{
+	char minus = '-';
+	int i;
+
+	if (n < 0)
+	{
+		_putchar(minus);
+		i = -1 * n;
+	}
+	else
+	{
+		i = n;
+	}
+	recurse(i);
+}
