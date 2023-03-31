ecb00f66581e6bc19d68cc5e36cec438f853d07d
Caleb Okyere <agyakojo4321@gmail.com>
2023-03-31 03:24:21 -0700

    resolving error

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,16 +13,21 @@ char *rot13(char *c)
 	int i = 0, j;
 	char s1[] = "abcdefghijklmnopqrstuvwxyz";
 	char s2[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char s4[] = "nopqrstuvwxyzabcdefghijklm";
+	char s3[] = "nopqrstuvwxyzabcdefghijklm";
+	char s4[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	while (c[i])
 	{
 		for (j = 0; j < 26; j++)
 		{
-			if (c[i]  == s1[j] || c[i] == s2[j])
+			if (c[i]  == s1[j])
+			{
+				c[i] = s3[j];
+				break;
+			}
+			else if (c[i] == s2[j])
 			{
 				c[i] = s4[j];
-				break;
 			}
 		}
 		i++;
