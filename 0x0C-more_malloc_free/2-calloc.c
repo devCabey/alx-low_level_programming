d72cf9a253334373549f22f216f1a9187fdc7ee9
Caleb Okyere <agyakojo4321@gmail.com>
2023-04-14 03:38:11 -0700

    error with 2-calloc resolved

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,27 +13,17 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *ptr;
-	unsigned int i;
 
-	if (nmemb && size != 0)
+	if (nmemb != 0 && size != 0)
 	{
-		ptr = malloc(sizeof(unsigned int) * size);
+		ptr = malloc(nmemb * size);
 
 		if (ptr == NULL)
 		{
 			return (NULL);
 		}
-		else
-		{
-			i = 0;
-			while (i < size)
-			{
-				ptr[i] = nmemb;
-				i++;
-			}
-		}
-		free(ptr);
-		return (ptr); }
+		return (ptr);
+	}
 	else
 	{
 		return (NULL);
