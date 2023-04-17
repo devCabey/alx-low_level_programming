1fdb1931440ba2193a5d19f902ca99a3aa04eda4
Caleb Okyere <agyakojo4321@gmail.com>
2023-04-17 03:55:00 -0700

    new type struct dog created

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+/**
+ * struct dog - Short description
+ *
+ * @name: A string
+ *
+ * @age: A float
+ *
+ * @owner: A string
+ *
+ * Description: Longer description
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog;
