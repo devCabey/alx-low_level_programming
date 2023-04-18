1ca338e0d32c3b18f5ae4d23ba9be8481ff8d32a
Caleb Okyere <agyakojo4321@gmail.com>
2023-04-18 00:54:04 -0700

    .

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	printf("Name: %s\nAge: %f\nOwner: %s\n", 
+	printf("Name: %s\nAge: %f\nOwner: %s\n",
 			(*d).name == NULL ? "(nil)" : (*d).name,
 			(*d).age,
 			(*d).owner == NULL ? "(nil)" : (*d).owner
