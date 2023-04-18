252e275c52dd68fe7448aad010dc6e02dd81e40d
Caleb Okyere <agyakojo4321@gmail.com>
2023-04-18 00:33:30 -0700

    double inclusion error resolved

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - Short description
  *
@@ -9,16 +11,16 @@
  *
  * Description: Longer description
  */
+
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog;
-
+} dog;
 typedef dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+#endif /*DOG*/
