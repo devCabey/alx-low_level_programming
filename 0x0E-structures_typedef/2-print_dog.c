1728a02b1cde5f220f063027db071063377d8b5e
Caleb Okyere <agyakojo4321@gmail.com>
2023-04-18 00:52:52 -0700

    ninja code

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,21 +14,9 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if ((*d).name != NULL)
-	{
-		printf("Name: %s\n", (*d).name);
-	}
-	else
-	{
-		printf("nil\n");
-	}
-	printf("Age: %f\n", (*d).age);
-	if ((*d).owner != NULL)
-	{
-		printf("Owner: %s\n", (*d).owner);
-	}
-	else
-	{
-		printf("nil\n");
-	}
+	printf("Name: %s\nAge: %f\nOwner: %s\n", 
+			(*d).name == NULL ? "(nil)" : (*d).name,
+			(*d).age,
+			(*d).owner == NULL ? "(nil)" : (*d).owner
+			);
 }
