858b6f3d65f5020cfb508da26ef43108ed0fcacf
Caleb Okyere <agyakojo4321@gmail.com>
2023-04-19 16:28:35 -0700

    debuggging

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -19,13 +19,13 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(1);
+		return (1);
 	}
 	i = atoi(argv[1]);
 	if (i < 0)
 	{
 		printf("Error\n");
-		exit(2);
+		return (2);
 	}
 	f = (char *)main;
 	j = 0;
