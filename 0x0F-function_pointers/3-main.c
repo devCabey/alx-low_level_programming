260c75ff31785b5078b47c55b2ee355256f776ff
Caleb Okyere <agyakojo4321@gmail.com>
2023-04-19 16:40:39 -0700

    debugging

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,31 +14,5 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j, ans;
-	char *k;
-
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	i = atoi(argv[1]);
-	j = atoi(argv[3]);
-	k = argv[2];
-
-	if (get_op_func(k) == NULL || k[1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	if ((*k == '/' && j == 0) || (*k == '%' && j == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	ans = get_op_func(k)(i, j);
-	printf("%d\n", ans);
-
 	return (0);
 }
