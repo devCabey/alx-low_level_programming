10046f97bcc0aa6134f18eb126abc1bc1836e2cd
Caleb Okyere <agyakojo4321@gmail.com>
2023-04-19 02:48:27 -0700

    function that searchs for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+/**
+ * int_index - Searhes for an integer
+ *
+ * @array: An array of integers
+ *
+ * @size: An integer
+ *
+ * @cmp: A function pointer
+ *
+ * Return: Integer
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, j;
+
+	if (size <= 0)
+		return (-1);
+	i = 0;
+	while (i < size)
+	{
+		j = cmp(array[i]);
+		if (j != 0)
+		{
+			return (i);
+		}
+		i++;
+	}
+	return (-1);
+}
