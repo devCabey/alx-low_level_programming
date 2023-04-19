a1397883c8e8946165e85b8123c50c618d7e6463
Caleb Okyere <agyakojo4321@gmail.com>
2023-04-19 16:11:15 -0700

    all done

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 	j = atoi(argv[3]);
 	k = argv[2];
 
-	if (get_op_funct(k) == NULL || k[1] != '\0')
+	if (get_op_func(k) == NULL || k[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
