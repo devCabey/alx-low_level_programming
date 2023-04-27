9d8f395cfc69d9cbec4d2062c4acc1c61737fb47
Caleb Okyere <agyakojo4321@gmail.com>
2023-04-27 16:45:32 -0700

    64bit program in assembly that prints Hello, Holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+section .data
+    format db "Hello, Holberton\n", 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    push rbp
+    mov rbp, rsp
+
+    lea rdi, [format]
+    xor eax, eax
+    call printf
+
+    mov rsp, rbp
+    pop rbp
+    xor eax, eax
+    ret
