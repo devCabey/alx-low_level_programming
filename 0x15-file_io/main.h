47e32be25f458e6784b1c3834ff75f15b698aae5
Caleb Okyere <agyakojo4321@gmail.com>
2023-05-08 14:44:35 -0700

    error resolved

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,4 +2,9 @@
 #define _MAIN_H_
 
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+ssize_t read_textfile(const char *filename, size_t letters);
 #endif
